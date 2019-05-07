cd5a9baa63688fbb799147a2c8aba6a2e4f9cfc8
pmomot <petruha.momot@gmail.com>
2019-05-07 21:48:55 +0200

    task 1

---
+++ src/add-n.ts
@@ -0,0 +1,3 @@
+export function addN (n1: number) {
+    return (n2: number) => n1 + n2
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import '../style/index.scss';
+import { addN } from './add-n';
 
 (function(){
+    // task 1
+    const addEight = addN(8);
+    console.log(addEight(7));
+    console.log(addEight(100));
+    //
+
     document.addEventListener('DOMContentLoaded', () => {
-        console.log('started');
+        console.log('app started');
     });
 })();
